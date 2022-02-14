f4fd2c0b4489b332577b98f37823499965dc71b3
jhlee <tlehffkffk@gmail.com>
2022-02-14 16:57:56 +0900

    First Test Commit

--- app/src/main/java/com/jhlee/roomdbexample/MainActivity.kt
+++ app/src/main/java/com/jhlee/roomdbexample/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
-        return false
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
